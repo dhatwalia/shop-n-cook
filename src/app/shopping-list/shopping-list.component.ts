d9a69734813e0fcfee175b87d51f4f84a3172ffd
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-11-30 01:27:40 -0500

    Adds shopping list model and UI

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Eggplant', 3),
+    new Ingredient('Garlic Cloves', 12),
+    new Ingredient('Tomatoes', 6),
+    new Ingredient('Onions', 3),
+    new Ingredient('Cilantro', 1),
+    new Ingredient('Chili Powder', 3),
+    new Ingredient('Salt', 3),
+    new Ingredient('Vegetable Oil', 5),
+  ];
 
   constructor() { }
 
