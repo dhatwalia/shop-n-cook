37322d1eb1b799f79a89f9cd89cda637e1a572cb
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-12-28 15:30:55 -0500

    Resolves data before loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
 import { Component, NgModule } from "@angular/core";
-import {  Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeResolverService } from "./recipes/recipe-resolver.service";
 
 const appRoutes: Routes = [
-    { path : '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path : 'recipes', component: RecipeComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent },
-        { path: ':id/edit', component: RecipeEditComponent },
-    ] },
-    { path : 'shopping-list', component: ShoppingListComponent },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    {
+        path: 'recipes', component: RecipeComponent, children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
+            { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
+        ]
+    },
+    { path: 'shopping-list', component: ShoppingListComponent },
 ]
 
 @NgModule({
@@ -22,5 +25,5 @@ const appRoutes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {
-    
+
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -16,13 +16,13 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://recipe-book3-default-rtdb.firebaseio.com/recipes.json').pipe(map(recipes => {
+        return this.http.get<Recipe[]>('https://recipe-book3-default-rtdb.firebaseio.com/recipes.json').pipe(map(recipes => {
             // pipe is used to ensure we have an ingredients [] even when no ingredients are entered by the user
             return recipes.map(recipe => {
                 return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
             });
-        })).subscribe(recipes => {
+        }), tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        });
+        }));
     }
 }
