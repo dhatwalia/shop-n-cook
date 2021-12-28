94beb01f80cd796b7fd47c6bdc1d0847575924f6
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2021-12-28 13:47:06 -0500

    Makes data fetchable on Firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -10,6 +12,17 @@ export class DataStorageService {
         const recipes = this.recipeService.getRecipes();
         this.http.put('https://recipe-book3-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
             console.log(response);
+        });
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://recipe-book3-default-rtdb.firebaseio.com/recipes.json').pipe(map(recipes => {
+            // pipe is used to ensure we have an ingredients [] even when no ingredients are entered by the user
+            return recipes.map(recipe => {
+                return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+            });
+        })).subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
         });
     }
 }
