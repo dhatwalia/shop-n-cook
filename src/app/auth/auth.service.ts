180194cbf03a0f1e5675e961eba65eef13e73fd9
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-01-01 01:56:47 -0500

    Stores user data in memory

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { catchError, Subject, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     kind: string;
@@ -14,6 +15,8 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    user = new Subject<User>();
+
     constructor(private http: HttpClient) { }
 
     signup(email: string, password: string) {
@@ -21,7 +24,9 @@ export class AuthService {
             email: email,
             password: password,
             returnedSecureToken: true
-        }).pipe(catchError(this.errorHandle));
+        }).pipe(catchError(this.errorHandle), tap(resData => {
+            this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
     }
 
     login(email: string, password: string) {
@@ -29,7 +34,9 @@ export class AuthService {
             email: email,
             password: password,
             returnedSecureToken: true
-        }).pipe(catchError(this.errorHandle));
+        }).pipe(catchError(this.errorHandle), tap(resData => {
+            this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
     }
 
     private errorHandle(errorRes: HttpErrorResponse) {
@@ -46,5 +53,12 @@ export class AuthService {
             }
             return throwError(errorMessage);
         }
+    }
+
+    private handleAuth(email: string, userId: string, token: string, expiresIn: number) {
+        // The outer Date converts mili-seconds to Date object
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
     }
 }
