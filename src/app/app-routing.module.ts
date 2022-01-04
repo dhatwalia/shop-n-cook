6cab6e1ef48d1303f273e4b17dea34c70d606261
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-01-04 13:06:31 -0500

    Splits app into modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,12 @@
-import { Component, NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeResolverService } from "./recipes/recipe-resolver.service";
-import { AuthComponent } from "./auth/auth.component";
-import { AuthGaurd } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    {
-        path: 'recipes', component: RecipeComponent, canActivate: [AuthGaurd], children: [
-            { path: '', component: RecipeStartComponent },
-            { path: 'new', component: RecipeEditComponent },
-            { path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
-            { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
-        ]
-    },
-    { path: 'shopping-list', component: ShoppingListComponent },
-    { path: 'auth', component: AuthComponent },
 ]
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { AlertComponent } from "./alert/alert.component";
+import { DropDownDirective } from "./dropdown.directive";
+import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
+
+@NgModule({
+    declarations: [
+        AlertComponent,
+        LoadingSpinnerComponent,
+        DropDownDirective,
+    ],
+    imports: [
+        CommonModule,
+    ],
+    exports: [
+        AlertComponent,
+        LoadingSpinnerComponent,
+        DropDownDirective,
+        CommonModule,
+    ],
+    entryComponents: [AlertComponent]
+})
+export class SharedModule { }
