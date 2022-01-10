95882362341107a2aceac36c84e88dd8811e9ac7
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-01-09 23:58:51 -0500

    Adds NgRx actions and reducers

---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,22 @@
+import { Ingredient } from "../../shared/ingredient.model";
+
+import * as ShoppingListActions from "./shopping-list.actions";
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Eggplant', 3),
+        new Ingredient('Garlic Cloves', 12),
+        new Ingredient('Tomatoes', 6),
+        new Ingredient('Onions', 3),
+        new Ingredient('Cilantro', 1),
+        new Ingredient('Chili Powder', 3),
+        new Ingredient('Salt', 3),
+        new Ingredient('Vegetable Oil', 5),
+    ]
+};
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
+    switch (action.type) {
+        case ShoppingListActions.ADD_INGREDIENT: return { ...state, ingredients: [...state.ingredients, action.payload] };
+    }
+}
